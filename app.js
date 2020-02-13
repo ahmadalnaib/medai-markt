6b09627ddda4f40f609908d78f5f46a4ce06a52d
ahmedhabeeb73 <az73system@gmail.com>
2020-02-13 12:46:53 +0100

    add cardSection

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-$('.owl-carousel').owlCarousel({
-    margin:10,
+// $('.owl-carousel').owlCarousel({
+//     margin:10,
+//     loop:true,
+//     autoWidth:true,
+//     items:4
+// })
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
     loop:true,
-    autoWidth:true,
-    items:4
-})
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplayHoverPause:true
+});
+// $('.play').on('click',function(){
+//     owl.trigger('play.owl.autoplay',[5000])
+// })
+// $('.stop').on('click',function(){
+//     owl.trigger('stop.owl.autoplay')
+// })
